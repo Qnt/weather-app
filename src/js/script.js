85ce9ad62e778997195044d10b36da7c36f6733d
Ainur <lihodeikvint@gmail.com>
2024-03-28 10:34:32 +0500

    change search form logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
 import { getGeolocationData, getWeatherData } from './api.js';
 
-const handleSearchBtnClick = async () => {
-  const searchInputEl = document.querySelector('#search-input');
-  const cityName = searchInputEl.value;
+const search = async event => {
+  const cityName = event.target.value.trim().toLowerCase();
+  if (!cityName) {
+    console.log('Enter a city');
+    return;
+  }
   const geoData = await getGeolocationData(cityName);
   if (!geoData.results) {
     console.log('No geodata!');
@@ -12,13 +15,16 @@ const handleSearchBtnClick = async () => {
   const lat = geoData.results[0].latitude;
   const lon = geoData.results[0].longitude;
   const weatherData = await getWeatherData(lat, lon);
+  console.log(weatherData);
 };
 
 const init = () => {
-  const searchBtnEl = document.querySelector('.search');
-  searchBtnEl.addEventListener('click', event => {
-    event.preventDefault();
-    handleSearchBtnClick();
+  const searchInputEl = document.querySelector('#search-input');
+  searchInputEl.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      search(event);
+    }
   });
 };
 
